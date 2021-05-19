a36eb158118fe834f7f02961e8a6545c1c35ed27
William Wise <38678762+WilliamPWise@users.noreply.github.com>
2021-05-19 16:09:37 -0600

    Add files via upload

--- crazy_eights_main.py
+++ crazy_eights_main.py
@@ -2,11 +2,23 @@
 import random, os
 
 #important functions
-def clean_the_screen():
+
+def input_number(text="", error_text=""):
 	
-	'''A simple function to clean the screen after each round of turns.'''
+	'''Returns a user's input as an integer instead of a string, asks for input again if the input isn't an integer.'''
 	
-	os.system('cls' if os.name == 'nt' else 'clear')
+	user_input = input(text)
+	
+	while True:		
+		
+		try:
+			num_input = int(user_input)
+			break
+		
+		except ValueError:
+			user_input = input(error_text)
+			
+	return num_input 
 
 
 def new_deck():
@@ -28,209 +40,242 @@ def draw_cards(num_of_cards, card_deck):
 	
 	cards_drawn = []
 	for card in range(num_of_cards):
-		cards_drawn.append(card_deck[-1])
-		del card_deck[-1]
+		cards_drawn.append(card_deck[0])
+		del card_deck[0]
 	return cards_drawn
 
 
-def reshuffle_deck(card_deck, discard_pile):
-
-	'''Shuffles the Discard pile back into the deck, while removing any suit proxies and keeping the last played card'''
-	
-	proxy_cards = ["Spades", "Hearts", "Clubs", "Diamonds"]
-	last_card = discard_pile.pop(-1)
-	
-	if last_card in proxy_cards: # If the last card is an 8, this makes sure the 8 and the proxy doesn't get shuffled back in. 
-		last_proxy = last_card
-		last_card = discard_pile.pop(-1)
-	
-	for card in discard_pile:
-		card_deck.append(discard_pile.pop())
-	
-	for proxy in proxy_cards:
-		for card in card_deck:
-			if card == proxy:
-				card_deck.remove(proxy)
-	
-	card_mixer.shuffle(card_deck)
-	discard_pile.append(last_card)
-	
-	if "8" in last_card:
-		discard_pile.append(last_proxy)
-		
-	print("*No more cards were left in the deck! Discarded cards were shuffled back in.* \n")
-
-
 def is_card_playable(card):	
+	
 	'''Checks to see if the card value can be played into the discard pile, returns True if so, and False if not.'''
 	
 	card_value = False
 	card_attributes = card.split(" of ")
-	card_type = card_attributes[0]
+	card_name = card_attributes[0]
 	card_suit = card_attributes[1]
 	
-	if card_type in discard_pile[-1] or card_suit in discard_pile[-1] or card_type == "8":
+	if card_name in discard_pile[-1] or card_suit in discard_pile[-1] or card_name == "8":
 		card_value = True
 	
 	return card_value
 
 
-def cpu_turn(player, card_deck, discard_pile):
-
-	'''Recreates the events of a turn taken by computer players'''
+def playable_cards(hand):
 	
-	# Check to see if there are any playable cards, and list them out temporarily.
+	'''Returns a list of all the cards a player can play during their turn.'''
 	
-	playable_cards = []
-	can_play = False
-	for card in player_hands[player]:
+	play_list = []
+	for card in hand:
 		if is_card_playable(card) == True:
-			playable_cards.append(card)
-			can_play = True
-	
-	# If there are no available cards, draw cards until a playable one is found.
-	
-	draw_count = 0
-	
-	while can_play == False:
-		
-		if card_deck == []:
-			reshuffle_deck(card_deck, discard_pile)
-			
-		player_hands[player] += draw_cards(1, card_deck)
-		draw_count += 1
-		new_card = player_hands[player][-1]
-		if is_card_playable(new_card) == True:
-			playable_cards.append(new_card)
-			can_play = True
-				
-	
-	discard_pile.append(playable_cards[0])
-	player_hands[player].remove(playable_cards[0])
-	
-	# Dialogue of the cpu turn 
-	
-	if draw_count == 1:
-		print(player, "had to draw a card!")
-	if draw_count >= 2:
-		print(player, "had to draw", draw_count, "cards!")
-		
-	print(player, "played a", "**" + discard_pile[-1] + "**!")
-	print("Cards remaining:", len(player_hands[player]), "\n")
-	
-	if "8" in discard_pile[-1]:
-		suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
-		proxy_suit = suits[card_mixer.randrange(4)]  # Cpu players choose a random suit!		
-		discard_pile.append(proxy_suit)
-		print("The suit is now", proxy_suit + "!\n")
-		
+			play_list.append(card)
+	return play_list
 
-def user_turn(player, card_deck, discard_pile):
-	
-	'''Creates dialogue and input for the user to play a card during their turn'''
-	
-	# The generic interface
-	
-	print("Choose a card to play:\n")
-	card_number = 1
-	can_play_hand = False
-	for card in player_hands[player]:
-		print("|" + str(card_number) + "|:", card)
-		card_number += 1	
-		if is_card_playable(card) == True:
-			can_play_hand = True
 
-	# Conditions if the player's hand has at least one playable card. 
+def selected_card(player):
 	
-	while can_play_hand == True:
+	'''Returns the card that the player will play during their turn. For the user, it gives input to choose the card that this function returns, and a dialogue loop for an invalid choice.'''
+	
+	error_text = "That's not a valid choice. Type the number next to the card to select it, then press enter. "
+	while player == "You":
 		
-		selection = int(input("\n"))
-		
-		if 1 <= selection <= card_number:			
-			card_select = player_hands[player][selection - 1]						
-			if is_card_playable(card_select) == True:					
-				discard_pile.append(card_select)
-				player_hands[player].remove(card_select)		
+		selection = input_number("", error_text)		
+		if 1 <= selection <= len(player_hands[player]):
+			card_select = player_hands[player][selection - 1]
+			if card_select in playable_cards(player_hands[player]):
 				break				
 			else:
 				print("\nThat's not a playable card! Try again. ")
 		
 		else:
-			print("\nThat's not a valid choice. Type the number next to the card to select it, then press enter. ")
+			print(error_text)
 	
-	# If there were no playable cards in the first place, cards are auto-drawn and played.
+	if player != "You":
+		card_select = playable_cards(player_hands[player])[0]
 	
-	if can_play_hand == False:
+	return card_select
+
+
+def mulligans(list_option=""):
+	
+	'''Returns a number of cards to draw if the player has no playable cards. Optionally, it can return the list of mulligan cards to be drawn instead.'''
+	
+	new_cards = []
+	draw_count = 0
+	
+	while playable_cards(new_cards) == []:
+		if draw_count >= len(card_deck):
+			break
+		new_cards.append(card_deck[draw_count])
+		draw_count += 1
+	
+	if list_option == "list":
+		return new_cards
+	else:
+		return draw_count
+
+def proxy_suit(player):
+	
+	'''Returns a string that acts as a suit changer when an eight card gets played. It gets used like a card proxy on the discard pile.'''
+	
+	suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+	if player == "You":
+		message = "What suit would you like to change it to?\n\n|1|: Spades |2|: Hearts |3|: Clubs |4|: Diamonds"
+		error_message = "Type the number next to the suit you want, and press enter."
+		suit_choice = input_number(message, error_message)
 		
-		draw_confirmation = input("\nYou have no playable cards! Press enter to draw from the deck. ")
-		print("This is what you drew:\n")
-		draw_count = 0
+		while suit_choice != "" and 0 > int(suit_choice) > 5:
+			suit_choice = input_number(error_message, error_message)
 		
-		while True:
-			
-			if card_deck == []:
-				reshuffle_deck(card_deck, discard_pile)
-			
-			player_hands[player] += draw_cards(1, card_deck)
-			draw_count += 1
-			new_card = player_hands[player][-1]
-			print(new_card)
-			
-			if is_card_playable(new_card) == True:
-				confirmation = input("\nPress enter to play the last card you drew. ")
-				discard_pile.append(new_card)
-				player_hands[player].remove(new_card)
-				break
-		
-	if "8" in discard_pile[-1]:
-		suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
-		print("\n" + player, "played an", "**" + discard_pile[-1] + "**!\n")
-		print("What suit would you like to change it to?\n")
-		print("|1|: Spades |2|: Hearts |3|: Clubs |4|: Diamonds")
-		while True:
-			suit_choice = int(input())
-			if 1 <= suit_choice	<= 4:
-				proxy_suit = suits[suit_choice - 1]
-				break
-			else:
-				print("Type the number next to the suit you want, and press enter.")
-		
-		discard_pile.append(proxy_suit)
-		clean_the_screen()
-		print("\nThe suit is now", proxy_suit + "!\n")
+		return suits[suit_choice - 1]
 	
 	else:
-		clean_the_screen()
-		print("\n" + player, "played a", "**" + discard_pile[-1] + "**!\n") 
+		return suits[card_mixer.randrange(4)]
+
+
+def mulligan_text(player, mulligans, mulligan_list):
+	
+	'''Returns or displays text for when a player has to draw from the deck.'''
+	
+	if player == "You":
+		confirm = input("You have no playable cards this turn! Press enter to draw from the deck. ")
+		text = "This is what you drew:\n\n"
+		for card in mulligan_list:
+			text += card + "\n"
+
+		if playable_cards(mulligan_list) > []:
+			text += "\nPress enter to play the last card you drew. "
+			confirm2 = input(text)
+			return ""
 		
+		else:
+			return text
+		
+	elif mulligans == 1:
+		return player + " had to draw a card!/n"
+		
+	elif mulligans >= 2:
+		return player + " had to draw " + str(mulligans) + " cards!/n"
+	
+	else:
+		return ""	
+
+
+def cardplay_text(player):
+	
+	'''Returns text to be printed when a card is played.'''
+	
+	text = player + " played a"
+	
+	if "8" in discard_pile[-1] or "Ace" in discard_pile[-1]:
+		text += "n" 
+	
+	text += " **" + discard_pile[-1] + "!**\n"
+	
+	if player != "You":
+		hand_size = len(player_hands[player])
+		text += "Cards Remaining: " + str(hand_size) + "\n"
+
+	return text
+	
+	
+def passing_text(player):
+	
+	'''Returns text for when a player has to pass their turn'''
+	
+	if player == "You":
+		confirm = input("/n Sorry, the deck is empty, and you have no playable cards. Press enter to pass this turn.")
+		return ""
+	else:
+		return player + " couldn't play any cards and had to pass this turn!"
+
+
+def ui_text(player):
+	
+	'''Returns the generic user interface text for the user's turn. It shows all the cards in the user's hand in the process''' 
+		
+	text = "It's your turn!"
+	
+	if playable_cards(player_hands[player]) > []:
+		text += " Select a card to play:"
+	else:
+		text += " This is your hand:"
+		
+	text += "\n\n"
+	card_number = 1
+	for card in player_hands[player]:
+		text += "|" + str(card_number) + "|: " + card + "\n"
+		card_number += 1
+		
+	return text
+
+
+def clean_the_screen(player="You"):
+	
+	'''A simple function to clean the screen after each round of turns.'''
+	
+	if player == "You":
+		os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def player_turn(player, card_deck, discard_pile):
+	
+	if player == "You":
+		print(ui_text(player) )
+	
+	if playable_cards(player_hands[player]) > []:
+		card_select = selected_card(player)
+				
+	if playable_cards(player_hands[player]) == [] and card_deck > []:
+		
+		print( mulligan_text(player, mulligans(), mulligans("list") ) )
+		player_hands[player] += draw_cards(mulligans(), card_deck)
+		if playable_cards(player_hands[player]) > []:
+			card_select = playable_cards(player_hands[player])[0]
+		
+	if playable_cards(player_hands[player]) == [] and card_deck == []:
+		print(passing_text(player))
+		return None
+	
+	discard_pile.append(card_select)
+	player_hands[player].remove(card_select)
+	
+	if "8" not in discard_pile[-1]:	
+		clean_the_screen(player)
+		
+	print(cardplay_text(player))
+	
+	if "8" in discard_pile[-1]:
+		discard_pile.append(proxy_suit(player))
+		clean_the_screen(player)
+		print("The suit is now " + discard_pile[-1] + "!\n")
 
 
 # ***-----The Main Gameplay Code!-----***
 
-
-
 # Set up the active players, their hands, and their scorecards.
 
-player_names = ["You", "Player 2", "Player 3", "Player 4"]
+player_names = ["You", "Player 2", "Player 3", "Player 4", "Player 5"]
 active_players = []
 player_hands = {}
 scorecards = {}
 
-
-print("***Welcome to the Crazy Eights Program!***\n")
-num_of_players = int(input("How many players would you like? "))
+greet_text = "***Welcome to the Crazy Eights Program!***\n\nHow many players would you like? "
+error_text = "Choose between 2, 3, 4, or 5 Players! "
+num_of_players = input_number(greet_text, error_text)
 
 while True:
 	
-	if 4 >= num_of_players >= 2:
+	if 5 >= num_of_players >= 2:
 		for index in range(num_of_players):
+			
 			active_players.append(player_names[index])
 			player_hands[player_names[index]] = []
 			scorecards[player_names[index]] = 0
+			
 		break
 	
 	else:
-		num_of_players = int(input("Choose between 2, 3, or 4 Players! "))
+		num_of_players = input_number(error_text, error_text)
 		
 # ****The Primary Game Loop****
 
@@ -261,7 +306,7 @@ while True:
 	
 	while True:
 		if "8" in discard_pile[0]:
-			card_deck.insert(0, discard_pile.pop())
+			card_deck.insert(-1, discard_pile.pop())
 			discard_pile += draw_cards(1, card_deck)
 			print("Oops! Let's try a diferent card. This time, it's a " + discard_pile[0] + "!\n")
 		else: 
@@ -280,10 +325,7 @@ while True:
 	player_index = 0
 	while True:	
 		active_player = active_players[player_index]
-		if active_player == "You":
-			user_turn(active_player, card_deck, discard_pile)
-		else:
-			cpu_turn(active_player, card_deck, discard_pile)
+		player_turn(active_player, card_deck, discard_pile) # This function contains all the actions of a player taking a turn.
 		player_index += 1
 		
 		if player_index == len(active_players):
@@ -305,7 +347,6 @@ while True:
 	clean_the_screen()
 	
 	scorecard_points = { "Ace": 1, "King": 10, "Queen": 10, "Jack": 10, "10": 10, "9": 9, "8": 50, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2 }	
-	
 	
 	print("Here are the point values of each card:\n")
 	print_interval = 0
